9dc51975814f45c49b9fbef74c8a9c39c6e32db8
李恭兆 <57301456@qq.com>
2019-02-25 10:28:28 +0800

    Update DBUtil.js

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 function createConnection () {
     var connect = mysql.createConnection({
-        host:'47.75.75.38',
+        host:'127.0.0.1',
         port:'3306',
         user:'root',
         password:'root',
